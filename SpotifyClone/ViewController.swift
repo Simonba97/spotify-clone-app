bd8c7443e5fac812b348e787e7d12bf917372b61
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-07-17 12:19:20 -0500

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Simón Bustamante Alzate on 17/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
